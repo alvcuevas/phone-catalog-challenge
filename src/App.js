44ae8ca8e1f4114491bac91e2bdc2ec3403fe378
Álvaro Cuevas <arcs@gft.com>
2021-01-13 13:29:03 +0100

    feat: dark theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomeView from './views/home';
-import DetailView from './views/detail';
-import NavigationBar from './components/navigation-bar';
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomeView from "./views/home";
+import DetailView from "./views/detail";
+import NavigationBar from "./components/navigation-bar";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { blue, indigo } from '@material-ui/core/colors';
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const handleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: darkTheme ? "dark" : "light",
+          primary: blue,
+          secondary: indigo
+        },
+      }),
+    [darkTheme]
+  );
+
   return (
-    <BrowserRouter>
-      <div>
-        <NavigationBar />
-        <Switch>
-          <Route exact path={'/'} component={HomeView} />
-          <Route path={'/details/:id'} component={DetailView} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <div>
+          <NavigationBar handleTheme={handleTheme} isDark={darkTheme} />
+          <Switch>
+            <Route exact path={"/"} component={HomeView} />
+            <Route path={"/details/:id"} component={DetailView} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- src/components/navigation-bar/index.js
+++ src/components/navigation-bar/index.js
@@ -1,31 +1,43 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import BrightnessHighIcon from "@material-ui/icons/BrightnessHigh";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1
-  }
+    flexGrow: 1,
+  },
 }));
 
-export default function NavigationBar() {
+export default function NavigationBar({ handleTheme, isDark }) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" color="secondary">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             Phone Catalog Challenge
           </Typography>
+          <Button
+            color="inherit"
+            onClick={handleTheme}
+            startIcon={!isDark ? <Brightness4Icon /> : <BrightnessHighIcon />}
+          >
+            Theme
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
