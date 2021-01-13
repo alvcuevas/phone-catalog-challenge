48e5e32c308ead6fdde657fe958757cb3cbbe00f
Álvaro Cuevas <arcs@gft.com>
2021-01-13 13:32:03 +0100

    feat: phone card component

--- src/components/phone-card/index.js
+++ src/components/phone-card/index.js
@@ -1,49 +1,82 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import {
+  makeStyles,
+  Card,
+  IconButton,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import ShareIcon from "@material-ui/icons/Share";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345
+    width: 310,
+    marginBottom: 40,
   },
   media: {
-    height: 140
-  }
-});
+    height: 280,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  actions: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+}));
 
 export default function PhoneCardComponent({ phone }) {
   const history = useHistory();
   const classes = useStyles();
 
-  const handlePhoneDetails = () => {
-    history.push(`/details/${phone.id}`);
-  };
+  const handlePhoneDetails = () => history.push(`/details/${phone.id}`);
+  const handleShare = () => alert("Sharing this awesome smartphone to your friends!");
 
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia className={classes.media} image={phone.img} title={phone.phone} />
-        <CardContent>
+        <CardMedia
+          className={classes.media}
+          image={phone.img}
+          title={phone.phone}
+        />
+        <CardContent className={classes.content}>
           <Typography gutterBottom variant="h5" component="h2">
-            {phone.phone}
+            <StarRateIcon /> {phone.phone}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            {phone.phone}
+            {phone.desc}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" onClick={handlePhoneDetails}>
-          SEE DETAILS
+      <CardActions className={classes.actions}>
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+          className={classes.button}
+          onClick={handlePhoneDetails}
+          startIcon={<SearchIcon />}
+        >
+          {" "}
+          Details
         </Button>
+        <IconButton aria-label="share" onClick={handleShare}>
+          <ShareIcon />
+        </IconButton>
       </CardActions>
     </Card>
   );
