040548aff4fba0158ec80f5da41ea4a0375af297
Álvaro Cuevas <arcs@gft.com>
2021-01-13 13:45:36 +0100

    style: responsive details

--- src/views/detail/index.js
+++ src/views/detail/index.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
 import { useSelector, useDispatch } from "react-redux";
-import Button from "@material-ui/core/Button";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { fetchPhoneDetails } from "../../store/actions/catalogActions";
-import Typography from '@material-ui/core/Typography';
-import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+
+import {
+  makeStyles,
+  Button,
+  Typography,
+  useMediaQuery
+} from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid"; 
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -22,29 +26,36 @@ const useStyles = makeStyles((theme) => ({
     borderTop: "1px solid lightgray",
     borderBottom: "1px solid lightgray",
   },
+  reducedDetailsContent: {
+    display: "flex",
+    flexDirection: 'column',
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 25
+  },
   imgSection: {
     borderRight: "1px solid lightgray",
     padding: 20,
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center",
   },
   detailsSection: {
     padding: 20,
   },
   brand: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     color: "#ff5722",
     letterSpacing: 1.5,
-    marginBottom: 15
+    marginBottom: 15,
   },
   phoneIcon: {
-    marginRight: 15
+    marginRight: 15,
   },
   feature: {
     marginBottom: 5,
-    paddingLeft: 10
-  }
+    paddingLeft: 10,
+  },
 }));
 
 const DetailView = () => {
@@ -52,6 +63,7 @@ const DetailView = () => {
   const params = useParams();
   const classes = useStyles();
   const history = useHistory();
+  const smallScreen = useMediaQuery('(max-width:650px)');
   const { details } = useSelector((store) => store.catalog);
   const phone = details[0];
 
@@ -65,84 +77,125 @@ const DetailView = () => {
   };
 
   const renderPhoneDetails = () => (
-    <div className={classes.container}>
-      <article className={classes.detailsContent}>
-        <section className={classes.imgSection}>
-          <img src={phone.img} alt={phone.desc} />
-        </section>
+      <div className={classes.container}>
+        <article className={`${smallScreen ? classes.reducedDetailsContent : classes.detailsContent}`}>
+          <section className={classes.imgSection}>
+            <img src={phone.img} alt={phone.desc} />
+          </section>
 
-        <section className={classes.detailsSection}>
-          <Typography className={classes.brand} gutterBottom variant="h6" component="h2">
-            <PhoneAndroidIcon className={classes.phoneIcon} /> {phone.phone}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h6">
-            Description
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.desc}  
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h6">
-            Price
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.price}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Network
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.network}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Dimensions
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.dimensions}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Weight
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.weight}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Screen Technology
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.type}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Screen Size
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.size}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Screen Resolution
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.resolution}
-          </Typography>
-          <Typography className={classes.feature} variant="h6" component="h2">
-            Color
-          </Typography>
-          <Typography className={classes.feature} variant="body1" color="textSecondary">
-            {phone.colors}
-          </Typography>
-        </section>
-      </article>
-      <Button
-        variant="outlined"
-        color="primary"
-        size="small"
-        className={classes.button}
-        onClick={handleReturn}
-        startIcon={<ArrowBackIcon />}
-      >
-        {" "}
-        Back to Catalog
-      </Button>
-    </div>
+          <section className={classes.detailsSection}>
+            <Typography
+              className={classes.brand}
+              gutterBottom
+              variant="h6"
+              component="h2"
+            >
+              <PhoneAndroidIcon className={classes.phoneIcon} /> {phone.phone}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h6">
+              Description
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.desc}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h6">
+              Price
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.price}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Network
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.network}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Dimensions
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.dimensions}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Weight
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.weight}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Screen Technology
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.type}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Screen Size
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.size}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Screen Resolution
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.resolution}
+            </Typography>
+            <Typography className={classes.feature} variant="h6" component="h2">
+              Color
+            </Typography>
+            <Typography
+              className={classes.feature}
+              variant="body1"
+              color="textSecondary"
+            >
+              {phone.colors}
+            </Typography>
+          </section>
+        </article>
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+          className={classes.button}
+          onClick={handleReturn}
+          startIcon={<ArrowBackIcon />}
+        >
+          {" "}
+          Back to Catalog
+        </Button>
+      </div>
   );
 
   return <>{!!details.length && renderPhoneDetails()}</>;
