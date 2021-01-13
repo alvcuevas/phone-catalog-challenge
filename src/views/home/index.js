8c07e843d353637dec28c4409fcdaeffd99a2ed9
Álvaro Cuevas <arcs@gft.com>
2021-01-13 13:32:22 +0100

    feat: home view

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,20 +2,28 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCatalog } from '../../store/actions/catalogActions';
 import PhoneListContainer from '../../components/phone-list';
+import Spinner from '../../components/spinner';
+import ErrorAlert from '../../components/error-alert'
 
 const HomeView = () => {
   const dispatch = useDispatch();
   const { catalog, loading, error } = useSelector(store => store.catalog);
 
   useEffect(() => {
-    dispatch(fetchCatalog());
-  }, []);
+    if (!catalog.length) {
+      dispatch(fetchCatalog());
+    }
+  }, [catalog]);
+
+  const renderPhones = () => <PhoneListContainer list={catalog} />
+  const renderLoading = () => <Spinner />
+  const renderError = () => <ErrorAlert />
 
   return (
     <>
-      {loading && 'Loading phone catalog...'}
-      {!!catalog.length && <PhoneListContainer list={catalog} />}
-      {error && 'Error fetching catalog'}
+      {loading && renderLoading()}
+      {!!catalog.length && renderPhones()}
+      {error && renderError()}
     </>
   );
 };
