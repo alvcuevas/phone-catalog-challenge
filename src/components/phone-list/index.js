33bea5f31aac1dffeeda62a0aa9356d226f9e322
Álvaro Cuevas <arcs@gft.com>
2021-01-13 13:31:55 +0100

    feat: phone list component

--- src/components/phone-list/index.js
+++ src/components/phone-list/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import PhoneCardComponent from '../phone-card';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import PhoneCardComponent from "../phone-card";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  catalog: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    width: '50%',
+    marginTop: 40
+  },
+}));
 
 const PhoneListContainer = ({ list }) => {
+  const classes = useStyles();
+
   return (
-    <>
-      {list.map(phone => (
-        <PhoneCardComponent key={phone.id} phone={phone} />
-      ))}
-    </>
+    <div className={classes.container}>
+      <div className={classes.catalog}>
+        {list.map((phone) => (
+          <PhoneCardComponent key={phone.id} phone={phone} />
+        ))}
+      </div>
+    </div>
   );
 };
 
