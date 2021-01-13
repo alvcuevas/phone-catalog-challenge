fae2da1480a3209073fc8971103710e162dd55cb
Alvaro <alvarucp@gmail.com>
2021-01-13 19:15:12 +0100

    minor changes

--- client/src/components/navigation-bar/index.js
+++ client/src/components/navigation-bar/index.js
@@ -1,34 +1,33 @@
-import React from "react";
-import {
-  makeStyles,
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-} from "@material-ui/core";
-import Brightness4Icon from "@material-ui/icons/Brightness4";
-import BrightnessHighIcon from "@material-ui/icons/BrightnessHigh";
+import React from 'react';
+import { makeStyles, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
     flexGrow: 1,
-  },
+    cursor: 'pointer'
+  }
 }));
 
 export default function NavigationBar({ handleTheme, isDark }) {
   const classes = useStyles();
+  const history = useHistory();
+
+  const handleReturn = () => history.push(`/`);
 
   return (
     <div className={classes.root}>
       <AppBar position="static" color="secondary">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className={classes.title} onClick={handleReturn}>
             Phone Catalog Challenge
           </Typography>
           <Button
